0071c66a92236a86c761e5184be8c8c070db99a1
Hanna Bergland <hannastrezh@gmail.com>
2018-09-18 15:17:10 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,7 +2,11 @@ class Dog
   def name = (dog_name) 
     this_dog_name = dog_name 
   end 
+  #takes an argument ans sets it equal to this_dog_name
+  #SETTING
   def name 
     this_dogs_name
   end 
-  
+  #this method for reading the name 
+  #GETTING
+ end 
