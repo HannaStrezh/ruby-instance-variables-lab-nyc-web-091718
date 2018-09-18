55c138ad6091cc1bb09c8549dfc91da0effa4897
Hanna Bergland <hannastrezh@gmail.com>
2018-09-18 15:22:39 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,17 +1,19 @@
 class Dog 
   def name = (dog_name) 
-    this_dogs_name = dog_name 
+    @this_dogs_name = dog_name 
   end 
   #takes an argument ans sets it equal to this_dog_name
   #SETTING
   def name 
-    this_dogs_name
+    @this_dogs_name
   end 
   #this method for reading the name 
   #GETTING
  end 
  
    lassie = Dog.new 
+   
    lassie.name = "Lassie"
+   
    lassie.name
    
