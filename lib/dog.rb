1e20bc64a9a3ff919113394003314d55fd8efe81
HannaStrezh <hannastrezh@gmail.com>
2018-11-02 12:09:15 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,19 +1,51 @@
-class Dog 
-  def name= (dog_name) 
-    @this_dogs_name = dog_name 
-  end 
-  #takes an argument ans sets it equal to this_dog_name
-  #SETTING
-  def name 
-    @this_dogs_name
-  end 
-  #this method for reading the name 
-  #GETTING
- end 
- 
-   lassie = Dog.new 
-   
-   lassie.name = "Lassie"
-   
-   puts lassie.name
-   
+class Dog
+    def name=(d_name)
+      @this_dogs_name = d_name
+    end
+
+    def name
+        @this_dogs_name
+    end
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class Dog
+#   def name= (dog_name)
+#     @this_dogs_name = dog_name
+#   end
+#   #takes an argument ans sets it equal to this_dog_name
+#   #SETTING
+#   def name
+#     @this_dogs_name
+#   end
+#   #this method for reading the name
+#   #GETTING
+#  end
+#
+#    lassie = Dog.new
+#
+#    lassie.name = "Lassie"
+#
+#    puts lassie.name
+#
